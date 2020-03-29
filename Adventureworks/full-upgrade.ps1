f50c0c5d1d3dc50411505942b55eaa33fb19fa03
Jelle Bens <jelle.bens@avanade.com>
2020-03-29 14:25:17 +0200

    refactored helm charts to adhere to principle of being able to deploy services independently

--- Adventureworks/full-upgrade.ps1
+++ Adventureworks/full-upgrade.ps1
@@ -1,7 +1,9 @@
 $tag=Get-Date -Format 'yymmddhhmmss'; 
 
+Write-Host "Building containers"
 docker build -f ".\Adventureworks.Web\Dockerfile" . -t adventureworks/web:$tag --build-arg VERSION=$tag
 docker build -f ".\Adventureworks.Orders\Dockerfile" . -t adventureworks/orders:$tag --build-arg VERSION=$tag
 
 Write-Host "Helm upgrade"
-helm upgrade adventureworks .\Helm\adventureworks\ --namespace adventureworks --set frontend.image.tag=$tag --set orders.image.tag=$tag
+helm upgrade frontend .\Helm\adventureworks\charts\frontend --namespace adventureworks --set image.tag=$tag
+helm upgrade service.orders .\Helm\adventureworks\charts\orders --namespace adventureworks --set image.tag=$tag
