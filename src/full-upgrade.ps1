fc871b4e17e784fe91ee0c7758aa020224453408
Jelle Bens <jelle.bens@avanade.com>
2020-03-29 15:27:45 +0200

    Scripted Istio gateway and virtual service

--- src/full-upgrade.ps1
+++ src/full-upgrade.ps1
@@ -1,9 +1,11 @@
-$tag=Get-Date -Format 'yymmddhhmmss'; 
+$command="upgrade"
+
+$tag=Get-Date -Format 'yymmdd.hh.mmss'; 
 
 Write-Host "Building containers"
 docker build -f ".\Adventureworks.Web\Dockerfile" . -t adventureworks/web:$tag --build-arg VERSION=$tag
 docker build -f ".\Adventureworks.Orders\Dockerfile" . -t adventureworks/orders:$tag --build-arg VERSION=$tag
 
 Write-Host "Helm upgrade"
-helm upgrade frontend .\Helm\adventureworks\charts\frontend --namespace adventureworks --set image.tag=$tag
-helm upgrade service.orders .\Helm\adventureworks\charts\orders --namespace adventureworks --set image.tag=$tag
+helm $command frontend .\Helm\adventureworks\charts\frontend --namespace adventureworks --set image.tag=$tag
+helm $command service.orders .\Helm\adventureworks\charts\orders --namespace adventureworks --set image.tag=$tag
